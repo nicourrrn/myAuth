5cca97b1a80629b6ed09157b9a8029bc784b323f
Vadym Sorokin <nicourrrn@gmail.com>
2021-12-26 18:26:11 +0200

    Финальный коммит примера

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/register", Register)
-	http.HandleFunc("/getme", GetMe)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/register", Register)
+	//mux.HandleFunc("/getme", GetMe)
+	mux.Handle("/getme", Logging(http.HandlerFunc(GetMe)))
+	mux.HandleFunc("/refresh", Refresh)
+	http.ListenAndServe("localhost:8080", mux)
 }
 
 func Refresh(w http.ResponseWriter, r *http.Request) {
@@ -22,17 +25,26 @@ func Refresh(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	var body map[string]string
-	if json.NewDecoder(r.Body).Decode(&body) != nil {
-		http.Error(w, "Error body", http.StatusBadRequest)
-		return
-	}
-	refEnc := GetFromBearer(body["refresh_token"])
-	if accCookie.Value == "" || refEnc == "" {
+	refData := GetFromBearer(r.Header.Get("refresh_token"))
+	if accCookie.Value == "" || refData == "" {
 		http.Error(w, "req not contain tokens", http.StatusBadRequest)
 		return
 	}
-	RefreshToken(refEnc, accCookie.Value, RefreshKey, AccessKey)
+	newAcc, newRef, err := RefreshToken(refData, accCookie.Value, RefreshKey, AccessKey)
+	if err != nil {
+		http.Error(w, "My error", http.StatusBadRequest)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:     "access_token",
+		Value:    newAcc,
+		HttpOnly: true,
+	})
+	data, _ := json.Marshal(map[string]string{
+		"refresh_token": newRef,
+	})
+	w.Write(data)
+
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
@@ -90,10 +102,6 @@ func GetMe(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
-	//if auth.ExpiresAt < time.Now().Unix(){
-	//	http.Error(w, "Old token, go to /refresh", http.StatusUnauthorized)
-	//	return
-	//}
 	w.WriteHeader(http.StatusOK)
 	file, err := os.Open(auth.Id)
 	if err != nil {
@@ -101,4 +109,12 @@ func GetMe(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	io.Copy(w, file)
+}
+
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, req)
+		log.Printf("%s %s %s", req.Method, req.RequestURI, time.Since(start))
+	})
 }
